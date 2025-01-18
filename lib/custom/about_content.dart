c17faa93babc52ca0d72aa40c6564962f567b46e
Donostia12 <suncry7890@gmail.com>
2025-01-18 23:03:08 +0800

    about new

--- lib/custom/about_content.dart
+++ lib/custom/about_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class AboutContent extends StatelessWidget {
+class AboutContent extends StatefulWidget {
   final String titleAbout;
   final String shortDescAbout;
   final String imageUrlAbout;
@@ -14,6 +15,40 @@ class AboutContent extends StatelessWidget {
   });
 
   @override
+  _AboutContentState createState() => _AboutContentState();
+}
+
+class _AboutContentState extends State<AboutContent> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadHtmlString('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <style>
+            body { margin: 0; padding: 0; }
+            iframe { width: 100%; height: 50vh; border: 0; }
+          </style>
+        </head>
+        <body>
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7620.457024738037!2d115.21167400000002!3d-8.716168!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd246b2ca586587%3A0xa03facb8882128fe!2sBali%20Gatra%20Komunikasi!5e1!3m2!1sen!2sid!4v1737102210557!5m2!1sen!2sid"
+            allowfullscreen
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
+        </body>
+        </html>
+      ''');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.all(16.0),
@@ -25,7 +60,7 @@ class AboutContent extends StatelessWidget {
             height: 200,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage(imageUrlAbout),
+                image: NetworkImage(widget.imageUrlAbout),
                 fit: BoxFit.cover,
               ),
             ),
@@ -43,14 +78,18 @@ class AboutContent extends StatelessWidget {
                 ),
                 const SizedBox(height: 8),
                 Text(
-                  contentAbout,
+                  widget.contentAbout,
                   textAlign: TextAlign.justify,
                 ),
                 const SizedBox(height: 16),
               ],
             ),
           ),
-          // Konten Berita Utama
+          // Google Maps Embed dengan HTML langsung dalam WebView
+          SizedBox(
+            height: 200,
+            child: WebViewWidget(controller: _controller),
+          ),
         ],
       ),
     );
