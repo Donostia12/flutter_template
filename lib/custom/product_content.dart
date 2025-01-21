5eb700386a1d4e715e5fcd51afc468469e8895e3
Donostia12 <suncry7890@gmail.com>
2025-01-21 09:43:15 +0800

    new retry image

--- lib/custom/product_content.dart
+++ lib/custom/product_content.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:template_scaffold/http/retry_image.dart';
 import 'package:template_scaffold/screen/product/detail_product.dart';
 
 class ProductContent extends StatelessWidget {
-  ProductContent(
-      {super.key,
-      required this.title,
-      required this.content,
-      required this.shortContent,
-      required this.img_url});
+  ProductContent({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.shortContent,
+    required this.img_url,
+  });
 
   final String title;
   final String content;
@@ -22,81 +23,60 @@ class ProductContent extends StatelessWidget {
       height: 200,
       child: Card(
         child: Row(
-          crossAxisAlignment:
-              CrossAxisAlignment.start, // Pastikan konten sejajar di atas
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Image.network(
-              img_url,
-              fit: BoxFit.cover,
-              width: 150,
-              height: 150,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              (loadingProgress.expectedTotalBytes ?? 1)
-                          : null,
-                    ),
-                  );
-                }
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Icon(
-                  Icons.error,
-                  size: 150,
-                  color: Colors.yellow,
-                ); // Menampilkan ikon error jika gambar gagal
-              },
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: RetryImageLoader(imgUrl: img_url),
             ),
+            Text(img_url),
             Expanded(
               child: Container(
-                padding: EdgeInsets.all(5), // Padding agar lebih nyaman dibaca
+                padding: EdgeInsets.all(5),
                 child: Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Agar teks rata kiri
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Text(
                       title,
                       style:
                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      maxLines: 2, // Batasi jumlah baris jika terlalu panjang
-                      overflow: TextOverflow
-                          .ellipsis, // Tambahkan "..." jika teks terlalu panjang
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    SizedBox(height: 5), // Jarak antara title dan shortContent
+                    SizedBox(height: 5),
                     Text(
                       shortContent.length > 80
                           ? "${shortContent.substring(0, 80)}..."
                           : shortContent,
-                      style: TextStyle(fontSize: 14), // Ukuran font lebih kecil
+                      style: TextStyle(fontSize: 14),
                     ),
-                    // Jarak sebelum tombolrigcenterRight
                     Align(
-                      alignment: Alignment.centerRight, // Tombol rata kanan
+                      alignment: Alignment.centerRight,
                       child: ElevatedButton(
                         onPressed: () {
                           Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => DetailProduct(
-                                      titleproduct: title,
-                                      shortDescproduct: shortContent,
-                                      imageUrlproduct: img_url,
-                                      contentproduct: content)));
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailProduct(
+                                titleproduct: title,
+                                shortDescproduct: shortContent,
+                                imageUrlproduct: img_url,
+                                contentproduct: content,
+                              ),
+                            ),
+                          );
                         },
                         style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 5, vertical: 5),
-                            textStyle: const TextStyle(fontSize: 14),
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.zero,
-                            ),
-                            side: const BorderSide(
-                                color: const Color.fromARGB(157, 4, 41, 84))),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 5, vertical: 5),
+                          textStyle: const TextStyle(fontSize: 14),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                          side: const BorderSide(
+                              color: Color.fromARGB(157, 4, 41, 84)),
+                        ),
                         child: Text('Read More'),
                       ),
                     ),
